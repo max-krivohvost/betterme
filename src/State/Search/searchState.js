77c3e704224b3911c1372ed5c911a14be2c2a60a
Maksim Kryvohvost <max.krivohvost@bizzabo.com>
2020-05-05 20:31:47 +0300

    Fixed paths

--- src/State/Search/searchState.js
+++ src/State/Search/searchState.js
@@ -6,18 +6,7 @@ import {
   SEARCH_REPOS,
   CLEAR_SEARCH,
   SET_LOADING,
-} from './searchState';
-
-let githubClientId;
-let githubClientSecret;
-
-if (process.env.NODE_ENV !== 'production') {
-  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
-} else {
-  githubClientId = process.env.GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-}
+} from './searchConst';
 
 const SearchState = (props) => {
   const initialState = {
